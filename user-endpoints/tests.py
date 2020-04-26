03102ab7bbc8ff3ca879e2fdae154a11bd7ec949
dannyallover <dannyhalawi15@gmail.com>
2020-04-25 17:12:30 -0700

    integrating twilio and modulating and refining code

--- user-endpoints/tests.py
+++ user-endpoints/tests.py
@@ -4,17 +4,28 @@ import json
 ### testing register of user
 def test_register_user():
     r = requests.post('http://localhost:5000/register',
-    json={"first_name": "Danny", "last_name": "Halawi", "phone": "650-766-9640", "zipcode": "94404"})
+    json={"first_name": "Danny", "last_name": "Halawi", "phone": "+16507400429", "zipcode": "94701"})
     assert r.status_code == 200
 
 ## testing register of another user
 def test_register_user_again():
     r = requests.post('http://localhost:5000/register',
-    json={"first_name": "Tammy", "last_name": "Halawi", "phone": "650-766-9601", "zipcode": "94404"})
+    json={"first_name": "Tammy", "last_name": "Halawi", "phone": "+16507669601", "zipcode": "94404"})
     assert r.status_code == 200
 
 ## testing update of user
 def test_update_user():
-    r = requests.put('http://localhost:5000/update/650-766-9640',
-    json={"first_name": "Danny", "last_name": "Halawi", "phone": "650-766-9640", "zipcode": "94701"})
+    r = requests.post('http://localhost:5000/update',
+    json={"first_name": "Danny", "last_name": "Halawi", "last_phone": "+16507400429", "new_phone": "+16507669640", "zipcode": "94701"})
+    assert r.status_code == 200
+
+## test unsubscribe user
+def test_unsubscribe_user():
+    r = requests.post('http://localhost:5000/unsubscribe',
+    json={"phone": "+16507669601"})
+    assert r.status_code == 200
+
+## test send
+def test_send():
+    r = requests.post('http://localhost:5000/send')
     assert r.status_code == 200
