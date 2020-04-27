c327475ea9c2be8308963348c62ebd5f43571d17
Kai Siebenrock <kai.siebenrock@berkeley.edu>
2020-04-27 10:24:14 -0700

    fix

--- js/script.js
+++ js/script.js
@@ -39,6 +39,33 @@
 //   });
 // });
 
+// Submit form
+// $('#form-submit').on('submit', (e) => {
+//
+//   e.preventDefault();
+//   console.log("click 1")
+//
+//   data = {};
+//   $("#form-submit > input").each(function(elem) {
+//     data[elem.name] = elem.value;
+//   });
+//
+//   console.log("click 2")
+//   $.ajax({
+//     type: "POST",
+//     url: "http://3.12.83.161:5000/register",
+//     data: data,
+//     dataType: "json",
+//     success: function(response) {
+//       alert("The server says: " + response);
+//     }
+//   });
+//
+// })
+
+
+
+
 // Show and hide forms for signup, update, unsubscribe
 const options = ["signup", "update", "unsubscribe"]
 $(document).on('click', 'button[id^="btn-"]', function(e) {
