8aade755937016c0f681d9e1c9adf9def42a77c3
Ananya Krishnaswamy <ananyaks2@gmail.com>
2020-04-26 20:55:37 -0700

    Update app.py

--- covid-data/app.py
+++ covid-data/app.py
@@ -133,4 +133,4 @@ def locate():
         return jsonify("Error while retrieving location")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=False)
