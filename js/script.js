f71cc0b581cec4fa3baaaf60a826b71f2681a694
Kai Siebenrock <kai.siebenrock@berkeley.edu>
2020-04-27 10:01:18 -0700

    Disable form validation, doesnt work

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
 // Validate form inputs
-$(function() {
-  var inputs = $("form#userForm input, form#userForm textarea");
+// $(function() {
+//   var inputs = $("form#userForm input, form#userForm textarea");
+//
+//   var validateInputs = function validateInputs(inputs) {
+//
+//     var validForm = true;
+//     inputs.each(function(index) {
+//       var input = $(this);
+//       if (!input.val() || (input.type === "radio" && !input.is(":checked"))) {
+//         $("#subuser").attr("disabled", "disabled");
+//         validForm = false;
+//       }
+//     });
+//
+//     return validForm;
+//   };
+//
+//   inputs.change(function() {
+//     if (validateInputs(inputs)) {
+//       $("#subuser").removeAttr("disabled");
+//     }
+//   });
+// });
 
-  var validateInputs = function validateInputs(inputs) {
 
-    var validForm = true;
-    inputs.each(function(index) {
-      var input = $(this);
-      if (!input.val() || (input.type === "radio" && !input.is(":checked"))) {
-        $("#subuser").attr("disabled", "disabled");
-        validForm = false;
-      }
-    });
-
-    return validForm;
-  };
-
-  inputs.change(function() {
-    if (validateInputs(inputs)) {
-      $("#subuser").removeAttr("disabled");
-    }
-  });
-});
+// $('#form-signup').submit(function(e) {
+//   console.log("click 1")
+//   e.preventDefault();
+//   console.log("click 2")
+//   $.ajax({
+//     url: "http://3.12.83.161:5000/register",
+//     data: $(this).serialize(),
+//     type: "POST",
+//     success: function(data) {
+//       console.log("success")
+//       alert(data);
+//     }
+//   });
+// });
 
 // Show and hide forms for signup, update, unsubscribe
 const options = ["signup", "update", "unsubscribe"]
